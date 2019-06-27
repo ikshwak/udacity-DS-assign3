a8de4dfeae05987ba2ed10ee702e3a2c923fc5fc
ikan <ikshwak.kandi@u-blox.com>
2019-06-27 15:43:40 -0700

    first commit

---
+++ problem1.py
@@ -0,0 +1,33 @@
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+    if number == 0 or number ==1:
+        return number
+
+    start = 1
+    end = number
+    sqrt = 0
+    while start <= end:
+        mid = (start+end)//2
+        if mid*mid == number:
+            sqrt = mid
+            return sqrt
+        elif mid*mid < number:
+            start = mid + 1
+            sqrt = mid
+        else:
+            end = mid - 1
+    return sqrt
+
+
+print ("Pass" if  (3 == sqrt(9)) else "Fail")
+print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (1 == sqrt(1)) else "Fail")
+print ("Pass" if  (5 == sqrt(27)) else "Fail")
---
+++ problem6.py
@@ -0,0 +1,38 @@
+class minmax:
+    def __init__(self,val1='',val2=''):
+        self.max = val1
+        self.min = val2
+
+
+def get_min_max(ints):
+    """
+    Return a tuple(min, max) out of list of unsorted integers.
+
+    Args:
+       ints(list): list of integers containing one or more integers
+    """
+    if ints == None or len(ints) == 0:
+       return tuple(None,None)
+
+    if len(ints) == 1:
+        return tuple(ints[0], ints[0])
+
+    mmVal = minmax()
+
+    if ints[0] >= ints[1]:
+        mmVal.max = ints[0]
+        mmVal.min = ints[1]
+    else:
+        mmVal.min = ints[0]
+        mmVal.max = ints[1]
+
+    for intVal in ints:
+        if intVal <= mmVal
+
+## Example Test Case of Ten Integers
+import random
+
+l = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l)
+
+print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
